3488f44959220a943fc5e8c21fe04fa6b77cfff8
Pedro Henrique <83087869+PeedroHenriquee@users.noreply.github.com>
2023-03-25 20:23:50 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+//Faça um programa que soma quatro número inteiros e exibe o //resultado da soma no console.
+
+
+//variavéis  dos 3 numeros.
+
+let n1 = 10
+let n2  = 20
+let n3 =  11
+let n4 = 33
+
+//recebe a soma dos 4 numeros
+
+let somatorio = n1 + n2 + n3 + n4
+
+//imprimi na tela os resultados
+
+print(somatorio)
